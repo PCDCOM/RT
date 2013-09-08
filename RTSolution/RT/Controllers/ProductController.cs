584e4953b53b072a9c8d439eb74947b3cac8cb9d
ganesh_ramasamy <ganesh.ramasamy@gmail.com>
2013-09-08 12:30:00 +0800

    Checked in by Menaka Changes - Added Product CRED

--- RTSolution/RT/Controllers/ProductController.cs
+++ RTSolution/RT/Controllers/ProductController.cs
@@ -11,13 +11,119 @@ namespace RT.Controllers
     {
         private RestaurantEntities db = new RestaurantEntities();
 
-
-
         public ActionResult ListsByGroup(long id)
         {
-            List<Product> products = db.Products.Where(p => p.ProductGroupID == id).ToList();
+            List<Product> products = db.Products.Where(p => p.Id == id).ToList();
             return View(products);
         }
 
+        [OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
+        public ViewResult Index()
+        {
+            return View(db.Products.Where(x=>x.Status==true).ToList());
+        }
+
+        //
+        // GET: /Product/Details/5
+
+        [OutputCache(NoStore = true, Duration = 0, VaryByParam = "*")]
+        public ViewResult Details(int id)
+        {
+            Product product = db.Products.Single(p => p.Id == id);
+            return View(product);
+        }
+
+        //
+        // GET: /Product/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.ProductGroup = new SelectList(db.ProductGroups, "id", "Name");
+            ViewBag.Kitchen = new SelectList(db.Kitchens, "KitchenID", "KitchenName");
+            //ViewBag.MasterProduct = new SelectList(db.Products.Where(x => x.MasterProductID != null), "ProductID", "name");
+            return View();
+        }
+
+        //
+        // POST: /product/Create
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            ViewBag.ProductGroup = new SelectList(db.ProductGroups, "id", "Name");
+            ViewBag.Kitchen = new SelectList(db.Kitchens, "KitchenID", "KitchenName");
+
+            if (ModelState.IsValid)
+            {
+                product.CreatedDate = DateTime.Now;                
+                product.CreatedBy = HttpContext.User.Identity.Name;
+                product.MasterProductID = null;
+                product.Status = true;
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+           return View(product);
+        }
+
+        //
+        // GET: /product/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            ViewBag.ProductGroup = new SelectList(db.ProductGroups, "id", "Name");
+            ViewBag.Kitchen = new SelectList(db.Kitchens, "KitchenID", "KitchenName");
+
+            Product product = db.Products.Single(p => p.Id == id);
+            return View(product);
+        }
+
+        //
+        // POST: /product/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            ViewBag.ProductGroup = new SelectList(db.ProductGroups, "id", "Name");
+            ViewBag.Kitchen = new SelectList(db.Kitchens, "KitchenID", "KitchenName");
+           
+            if (ModelState.IsValid)
+            {
+                product.ModifiedDate = DateTime.Now;
+                product.MasterProductID = null;
+                db.Products.Attach(product);
+               
+               
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        //
+        // GET: /product/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            Product product = db.Products.Single(p => p.Id == id);
+            return View(product);
+        }
+
+        //
+        // POST: /product/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Single(p => p.Id == id);          
+            product.ModifiedDate = DateTime.Now;
+            product.Status = false;
+            //db.products.Attach(product);
+            db.Entry(product).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
